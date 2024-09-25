6054e0e61d494e48ff4a0f3d3242f357bb9ee2ee
Andrey M. <mashenko.andr@gmail.com>
2024-09-25 18:13:58 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function toggleMenu() {
     contentContainer.classList.toggle('menu-open'); // Lägger till/removerar klassen för innehållet
     profile.classList.toggle('menu-open'); // Lägger till/removerar klassen för profilsektionen
 
+    // Visa eller dölja menyn baserat på om 'open' klassen finns
     if (menuLinks.classList.contains('open')) {
         menuLinks.style.display = 'block'; // Visa menyn
     } else {
