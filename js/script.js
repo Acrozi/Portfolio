8ac5a1f8af5fa424a638f360ea29d8476ddfdbc7
Andrey M. <mashenko.andr@gmail.com>
2024-09-25 16:07:42 +0200

    + GitHub actions for automatic deployment

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const menuLinks = document.querySelector('.menu-links');
+    const contentContainer = document.getElementById('content-container');
+    const profile = document.getElementById('profile');
+
+    // Växla klassen för att öppna/stänga menyn
+    menuLinks.classList.toggle('open');
+    contentContainer.classList.toggle('menu-open'); // Lägger till/removerar klassen för innehållet
+    profile.classList.toggle('menu-open'); // Lägger till/removerar klassen för profilsektionen
+
+    if (menuLinks.classList.contains('open')) {
+        menuLinks.style.display = 'block'; // Visa menyn
+    } else {
+        menuLinks.style.display = 'none'; // Dölja menyn
+    }
+}
